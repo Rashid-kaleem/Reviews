587f0cdbfa0eaacf16415a34cef9279d724eb1cd
RASHID-KALEEM <rashidkaleem321@gmail.com>
2022-08-03 13:15:09 +0500

    Init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Review from './Review';
+
+
+function App() {
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h4>Reviews</h4>
+          <div className='underline'></div>
+        </div>
+        <Review></Review>
+       
+        
+
+      </section>
+
+
+
+    </main>
+  );
+}
+
+export default App;
